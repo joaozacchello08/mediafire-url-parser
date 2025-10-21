915d04cb754c2bb7f6a4f859da40ac9cacb8e27b
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-10-21 20:49:05 -0300

    better response for web browser

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
 from requests import get
 from bs4 import BeautifulSoup as bs
 
@@ -8,7 +9,7 @@ app = FastAPI()
 def root():
     return { "message": "Hello World!" }
 
-@app.get("/api/{mediafire_url:path}")
+@app.get("/api/{mediafire_url:path}", response_class=HTMLResponse or JSONResponse)
 def get_url(mediafire_url: str):
     if "https://www.mediafire.com" not in mediafire_url:
         return { "message": "Invalid URL." }, 400
@@ -21,7 +22,10 @@ def get_url(mediafire_url: str):
         if not element:
             raise AttributeError("Couldn't parse mediafire URL.")
         
+        # if everything ok
         result_url = element.get("href")
-        return { "message": "URL was parsed successfully!", "result": result_url }, 200
+        html_content = f'<html><head><title>your mediafire link is ready!</title></head><body><h1><a href="{result_url}">SEU LINK</a></h1></body></html>'
+        return html_content
+        # return { "message": "URL was parsed successfully!", "result": result_url }, 200
     except Exception as e:
         return { "message": f"An error occurred trying to parse mediafire URL: {str(e)}" }, 500
